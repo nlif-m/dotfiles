01bc47ab6d76899803d81dcd3814525f61d43c4a
mana1500 <levha150@gmail.com>
2021-01-26 19:01:04 +0300

    Update updateConfig.sh

--- updateConfig.sh
+++ updateConfig.sh
@@ -10,9 +10,11 @@ ln .vimrc/.vimrc ~/ 2>/dev/null
 
 # i3config
 mkdir -p ~/.config/i3
+mv ~/.config/i3/config ~/.config/i3/config.old
 ln i3-config/i3/config ~/.config/i3/config
 
 # i3status
 mkdir -p ~/.config/i3
+mv ~/.config/i3status/config ~/.config/i3status/config.old
 ln i3-config/i3status/config ~/.config/i3status/config
 
