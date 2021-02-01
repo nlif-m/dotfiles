ec790cf146c72f82124f5108a947c24b37c203bb
mana1500 <levha150@gmail.com>
2021-02-01 17:54:12 +0300

    Revert "BUGFIX:  Repair updateConfig"
    This reverts commit 0d1d76dfc1be2c47cf4506aef9d9a213b101045a.

--- updateConfig.sh
+++ updateConfig.sh
@@ -3,7 +3,6 @@
 
 
 # .zshrc
-rm ~/.zshrc.old 
 mv ~/.zshrc ~/.zshrc.old
 ln .zshrc ~/ 2>/dev/null
 
@@ -12,13 +11,11 @@ ln .vimrc/.vimrc ~/ 2>/dev/null
 
 # i3config
 mkdir -p ~/.config/i3
-rm ~/.config/i3/config.old
 mv ~/.config/i3/config ~/.config/i3/config.old
 ln i3-config/i3/config ~/.config/i3/config
 
 # i3status
 mkdir -p ~/.config/i3
-rm ~/.config/i3status/config.old 
 mv ~/.config/i3status/config ~/.config/i3status/config.old
 ln i3-config/i3status/config ~/.config/i3status/config
 
