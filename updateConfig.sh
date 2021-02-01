0d1d76dfc1be2c47cf4506aef9d9a213b101045a
mana1500 <levha150@gmail.com>
2021-02-01 17:22:02 +0300

    BUGFIX:  Repair updateConfig FEATURE: i3lock now with black screen also $mod + F10 run i3lock && systemctl suspend

--- updateConfig.sh
+++ updateConfig.sh
@@ -3,6 +3,7 @@
 
 
 # .zshrc
+rm ~/.zshrc.old 
 mv ~/.zshrc ~/.zshrc.old
 ln .zshrc ~/ 2>/dev/null
 
@@ -11,11 +12,13 @@ ln .vimrc/.vimrc ~/ 2>/dev/null
 
 # i3config
 mkdir -p ~/.config/i3
+rm ~/.config/i3/config.old
 mv ~/.config/i3/config ~/.config/i3/config.old
 ln i3-config/i3/config ~/.config/i3/config
 
 # i3status
 mkdir -p ~/.config/i3
+rm ~/.config/i3status/config.old 
 mv ~/.config/i3status/config ~/.config/i3status/config.old
 ln i3-config/i3status/config ~/.config/i3status/config
 
