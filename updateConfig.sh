63222cee9372eda8e271170a98d008c9c66c7f92
mana1500 <levha150@gmail.com>
2021-02-01 17:57:13 +0300

    BUGFIX: Really repair updateConfig FEATURE: Add telegram-desktop autostart and update $mod+F10 hotkey to i3lock && systemctl suspend

--- updateConfig.sh
+++ updateConfig.sh
@@ -3,19 +3,20 @@
 
 
 # .zshrc
-mv ~/.zshrc ~/.zshrc.old
+rm ~/.zshrc 
 ln .zshrc ~/ 2>/dev/null
 
 # .vimrc
+rm ~/.vimrc
 ln .vimrc/.vimrc ~/ 2>/dev/null
 
 # i3config
 mkdir -p ~/.config/i3
-mv ~/.config/i3/config ~/.config/i3/config.old
-ln i3-config/i3/config ~/.config/i3/config
+rm ~/.config/i3/config
+ln ~/code/dotfiles/i3-config/i3/config ~/.config/i3/config
 
 # i3status
 mkdir -p ~/.config/i3
-mv ~/.config/i3status/config ~/.config/i3status/config.old
-ln i3-config/i3status/config ~/.config/i3status/config
+rm ~/.config/i3status/config
+ln ~/code/dotfiles/i3-config/i3status/config ~/.config/i3status/config
 
