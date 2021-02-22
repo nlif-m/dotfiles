089559757937989691d84d1a920a818ccd1f6eb2
nlif <m.payunen@gmail.com>
2021-02-22 14:24:04 +0300

    fix: fix update config

--- updateConfig.sh
+++ updateConfig.sh
@@ -8,7 +8,7 @@ ln .zshrc ~/ 2>/dev/null
 
 # .vimrc
 rm ~/.vimrc
-ln .vimrc/.vimrc ~/ 2>/dev/null
+ln .vimrc ~/ 2>/dev/null
 
 # i3config
 mkdir -p ~/.config/i3 2>/dev/null
@@ -16,7 +16,7 @@ rm ~/.config/i3/config
 ln ~/code/dotfiles/i3-config/i3/config ~/.config/i3/config
 
 # i3status
-mkdir -p ~/.config/i3 2>/dev/null 
+mkdir -p ~/.config/i3status 2>/dev/null 
 rm ~/.config/i3status/config
 ln ~/code/dotfiles/i3-config/i3status/config ~/.config/i3status/config
 
