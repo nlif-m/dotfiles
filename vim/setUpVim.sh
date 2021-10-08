8f3369c53d20db86a33e60e626bb26ce83a189aa
nlif <m.payunen@gmail.com>
2021-10-08 21:38:13 +0300

    feat(setUpVim): add a lua support also with luajit, also mass refactor of code

--- vim/setUpVim.sh
+++ vim/setUpVim.sh
@@ -2,19 +2,22 @@
 folder=$(pwd)
 PREFIX=~/.local/src
 VIM_FOLDER=vim
-mkdir -p "$PREFIX"
-
-git clone https://github.com/vim/vim.git "$PREFIX"/"$VIM_FOLDER"
-
-cd "$PREFIX"/"$VIM_FOLDER"/src || echo "Error cd $PREFIX/src" ; exit
-
- ./configure --with-features=huge --with-python3-command=python3 --enable-python3interp=yes --with-compiledby=$USER
 
 
-make -j$(nproc) LDFLAGS="-rdynamic"
-sudo make -j$(nproc) install LDFLAGS="-rdynamic"
+vim_src="$PREFIX/$VIM_FOLDER"
+mkdir -p "$PREFIX" || exit 1
+[ ! -d "$vim_src" ] && (git clone https://github.com/vim/vim.git "$vim_src" || exit 2)
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd "$PREFIX"/"$VIM_FOLDER"/src || (echo "Error cd $PREFIX/src" && exit 3)
+
+ ./configure --with-features=huge --with-python3-command=python3 --enable-python3interp=yes --with-luajit --enable-luainterp=yes --with-compiledby=$USER
+
+
+make -j LDFLAGS="-rdynamic"
+sudo make -j install LDFLAGS="-rdynamic"
+
+vim_Vundle="$HOME/.vim/bundle/Vundle.vim"
+[ ! -d "$vim_Vundle" ] && (git clone https://github.com/VundleVim/Vundle.vim.git "$vim_Vundle" || exit 4)
 vim +PluginInstall +qall
 
 cd $folder
