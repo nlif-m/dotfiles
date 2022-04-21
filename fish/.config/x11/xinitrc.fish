7dd3acf479a9b05acd4aa26ca8e1886b570e3784
nlif <m.payunen@gmail.com>
2022-04-21 21:56:20 +0300

    [fish][xinitrc.fish] Make some refactor

--- fish/.config/x11/xinitrc.fish
+++ fish/.config/x11/xinitrc.fish
@@ -11,16 +11,35 @@ sh -c "setxkbmap -layout us,ru -option 'grp:ctrl_shift_toggle' -option 'grp:caps
 set wm $argv[1]
 
 
+
+function similar
+    redshift &
+    remaps &
+end
+
 function startup_i3 
-    sh -c "exec ssh-agent i3"
+    similar
+    exec ssh-agent i3
 end
     
 function startup_dwm
-    sh -c "exec dwm"
+    similar
+    exec dwm
 end
 
 function startup_xfce4 
+    similar
     exec startxfce4
+end
+
+function startup_plasma
+    similar
+    exec startplasma-x11
+end
+
+function startup_gnome
+    similar
+    exec gnome-session
 end
 
 echo $argv[1] >> $HOME/x.txt
@@ -28,13 +47,17 @@ echo $wm >> $HOME/x.txt
 
 switch $wm
     case i3 i3wm
-	startup_i3
+        startup_i3
     case dwm
-	startup_dwm
+        startup_dwm
     case xfce xfce4
-	startup_xfce4
+        startup_xfce4
+    case plasma kde
+        startup_plasma
+    case gnome
+        startup_gnome
     case '*'
-	startup_i3
+        startup_i3
 
 end
 
